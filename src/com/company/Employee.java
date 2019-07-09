984a3ce0e61565fe126e3500c7cbb637f40e038b
zsrbakova <zsrbakova@yahoo.com>
2019-07-09 10:11:27 +0200

    FirstCommit

---
+++ src/com/company/Employee.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Employee {
+    int ID;
+    String name;
+    String surname;
+    int salary;
+    String StartDate;
+
+    public Employee()
+    {
+
+    }
+    public Employee(int ID,String name,String surname,int salary,String StartDate)
+    {
+        this.ID=ID;
+        this.name=name;
+        this.surname=surname;
+        this.salary=salary;
+        this.StartDate=StartDate;
+    }
+
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee e1=new Employee(1,"Ana","Stojanova",30000,"01.02.2019");
+
+        System.out.println(e1.ID);
+        System.out.println(e1.name);
+        System.out.println(e1.surname);
+        System.out.println(e1.salary);
+        System.out.println(e1.StartDate);
+	// write your code here
+    }
+}
